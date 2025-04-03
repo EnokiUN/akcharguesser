354db1e4e924fd74ddeb76978cfbae4ff70895e6
Enoki <enokiun@gmail.com>
2025-04-03 11:37:53 +0300

    fix: handle eyja alter

--- src/util.rs
+++ src/util.rs
@@ -6,6 +6,7 @@ pub fn normalise_name(name: &str) -> String {
         "лето" => "leto".to_string(),
         "роса" => "rosa".to_string(),
         "позёмка" => "pozemka".to_string(),
+        "eyjafjallathehvítaska" => "eyjafjallathehvitaska".to_string(),
         name => name
             .replace(' ', "")
             .replace('-', "")
